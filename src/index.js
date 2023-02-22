303a85fd7d5997db4657d3e32847fb8da4d1fe40
Thomas Wallingford <tbwallingford@gmail.com>
2023-02-21 22:00:23 -0700

    Deliverables met. Stretches of delete and priorities met.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+function buildTodo(todo){
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  let priority = document.querySelector('#priority')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x'
+  p.textContent = `${todo} `
+  if(priority.value === 'high'){
+    p.style.color = '#FF0000'
+  }
+  else if(priority.value === 'medium'){
+    p.style.color = '#FFFF00'
+  }
+  else if(priority.value === 'low'){
+    p.style.color = '#00FF00'
+  }
+  p.appendChild(btn)
+  document.querySelector("#list").appendChild(p)
+
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
+
+document.querySelector('form').addEventListener('submit', (e) => {
+  e.preventDefault()
+  buildTodo(document.getElementById('new-task-description').value)
+  document.querySelector('#create-task-form').reset()
+})
